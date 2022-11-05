40c063a890268a0615476e535c5e8ec49288735b
Archie Verma <a1745070@student.adelaide.edu.au>
2022-11-05 16:50:13 +1030

    Creating a GUI frame with a heading GUI and frame size 700,700 and exit apllication on closing GUI

---
+++ GpsGui.java
@@ -0,0 +1,19 @@
+import nz.sodium.*;
+import javax.swing.*;
+import java.awt.FlowLayout;
+import swidgets.*;
+
+public class GpsGui {
+
+    public static void main(String[] args){
+
+        //GUI frame heading
+        JFrame frame = new JFrame("GUI");
+        //Frame Size
+        frame.setSize(700, 700);
+        //On Pressing GUI cross stop running the application
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        }
+    }
+
