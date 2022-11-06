5d3f144ffbcacda03f84c1af778c96ef15f9d439
Archie Verma <a1745070@student.adelaide.edu.au>
2022-11-06 14:41:18 +1030

    Print tracker with number and trying to fix error because its not printing Tracker 3

--- GpsGui.java
+++ GpsGui.java
@@ -27,25 +27,45 @@ public class GpsGui {
             //Printing the GPS Data on the terminal
             System.out.println(ev);
              });
+  
 
             //Printing Tracker with numbers
             Cell<String> tNum;
             tNum = s.map((GpsEvent ev) -> 
             {
             String[] expected;
-            //Using split to remove space after Tracker to directly print Latitude
-            expected = ev.toString().split(" ");
-            
+            //Using split to remove space to directly print Tracker
+            expected= ev.toString().split(" ");
             String t1String;
-            //Printing Latitute as it is at place 2 in the array
-            t1String = (expected[2]);
-            //t1String is printing Latitude
-            return t1String;  
+            //Printing Latitute as it is at place 0 in the array
+            t1String = (expected[0]);
+            //t1String is printing Tracker with number
+            return t1String; 
             }).hold(" ");
-            
             //Label for printing Latitude
             SLabel lA = new SLabel(tNum);
             frame.add(lA);
+
+
+            //Printing Latitude with numbers
+            Cell<String> lNum;
+            lNum = s.map((GpsEvent ev) -> 
+            {
+            String[] expected;
+            //Using split to remove space after Tracker to directly print Latitude
+            expected = ev.toString().split(" ");
+            String t2String;
+            //Printing Latitute as it is at place 2 in the array
+            t2String = (expected[2]);
+            //t2String is printing Latitude
+            return t2String;  
+            }).hold(" ");
+            //Label for printing Latitude
+            SLabel lB = new SLabel(lNum);
+            frame.add(lB);
+
+
+           
             //Layout for printing
             frame.setLayout(new FlowLayout());
             // Setting Gui as visible
