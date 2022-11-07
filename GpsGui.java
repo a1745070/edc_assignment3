b992d0e8b56e2e758afcbd3631f57cba7d34b375
Archie Verma <a1745070@student.adelaide.edu.au>
2022-11-07 17:24:37 +1030

    Making modification in the layout

--- GpsGui.java
+++ GpsGui.java
@@ -145,6 +145,10 @@ public class GpsGui {
         headCa.setPreferredSize(new Dimension(1300, 50));
         frame.getContentPane().add(headCa);
         frame.add(headCa);
+        String fil = new SimpleDateFormat("dd/MM/yyyy, hh:mm:ss").format(new Date());
+        JLabel  ak = new JLabel(fil);
+        ak.setForeground(Color.RED);
+        frame.add(ak);
 
         //Part 3 B Control Panel  
         JButton headCb = new JButton("Control Panel for Latitude and Longitude: ");
@@ -155,7 +159,7 @@ public class GpsGui {
         JLabel lLB = new JLabel("Latitude Lower Bound: ");
         lLB.setForeground(Color.RED);
         frame.add(lLB);
-        lLB .setPreferredSize(new Dimension(190, 90));
+        lLB .setPreferredSize(new Dimension(150, 90));
 
         STextField textA = new STextField("-90.000", 7);
         textA.setPreferredSize(new Dimension(400, 30));
@@ -164,7 +168,7 @@ public class GpsGui {
         JLabel lUB = new JLabel("Latitude Upper Bound: ");
         lUB.setForeground(Color.BLUE);
         frame.add(lUB );
-        lUB.setPreferredSize(new Dimension(190, 90));
+        lUB.setPreferredSize(new Dimension(150, 90));
 
         STextField textB = new STextField("90.000", 7);
         textB.setPreferredSize(new Dimension(400, 30));
@@ -173,7 +177,7 @@ public class GpsGui {
         JLabel loLB = new JLabel("Longitude Lower Bound: ");
         loLB.setForeground(Color.RED);
         frame.add(loLB );
-        loLB.setPreferredSize(new Dimension(190, 90));
+        loLB.setPreferredSize(new Dimension(160, 90));
 
         STextField textC = new STextField("180.000", 7);
         textC.setPreferredSize(new Dimension(400, 30));
@@ -182,14 +186,14 @@ public class GpsGui {
         JLabel loUB = new JLabel("Longitudes Upper Bound: ");
         loUB.setForeground(Color.BLUE);
         frame.add(loUB );
-        loUB .setPreferredSize(new Dimension(190, 90));
+        loUB .setPreferredSize(new Dimension(170, 90));
 
         STextField textD = new STextField("180.000", 7);
         textD.setPreferredSize(new Dimension(400, 30));
         frame.add(textD);
 
         JButton setR = new JButton("SET RANGE");
-        setR.setPreferredSize(new Dimension(200, 30));
+        setR.setPreferredSize(new Dimension(260, 30));
         setR.setForeground(Color.BLACK);
         frame.getContentPane().add(setR);
         frame.add(setR);
