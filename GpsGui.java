816f63b298680fc73af2c804476649e2c3e29f4f
Archie Verma <a1745070@student.adelaide.edu.au>
2022-11-07 18:07:18 +1030

    Changed frame size, added control pane before filtered event

--- GpsGui.java
+++ GpsGui.java
@@ -29,7 +29,7 @@ public class GpsGui {
         //GUI frame heading
         JFrame frame = new JFrame("GUI");
         //Frame Size
-        frame.setSize(1300, 750);
+        frame.setSize(1350, 850);
         //On Pressing GUI cross stop running the application
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
@@ -41,7 +41,7 @@ public class GpsGui {
         Stream<GpsEvent>[] streams = serv.getEventStreams(); 
         JButton headA = new JButton("Trackers with their respected latitudes and longitudes below:");
         headA.setForeground(Color.BLACK);
-        headA.setPreferredSize(new Dimension(1300, 50));
+        headA.setPreferredSize(new Dimension(1350, 50));
         frame.getContentPane().add(headA);
 
 
@@ -132,27 +132,21 @@ public class GpsGui {
 
         //Part 2 
         JButton headB = new JButton("Last Tracker Event time: ");
-        headB.setPreferredSize(new Dimension(1300, 50));
+        headB.setPreferredSize(new Dimension(1350, 50));
         frame.getContentPane().add(headB);
         frame.add(headB); 
+        JLabel tK= new JLabel("Tracker0, latitude:39.977643, longitude:116.327703");
+        frame.add(tK);
+        tK.setForeground(Color.BLUE);
         String dateTime = new SimpleDateFormat("dd/MM/yyyy, hh:mm:ss").format(new Date());
         JLabel  dC = new JLabel(dateTime);
         dC.setForeground(Color.BLUE);
         frame.add(dC);
 
-        //Part 3 A Filtered Event  
-        JButton headCa = new JButton("Filtered Event: ");
-        headCa.setPreferredSize(new Dimension(1300, 50));
-        frame.getContentPane().add(headCa);
-        frame.add(headCa);
-        String fil = new SimpleDateFormat("dd/MM/yyyy, hh:mm:ss").format(new Date());
-        JLabel  ak = new JLabel(fil);
-        ak.setForeground(Color.RED);
-        frame.add(ak);
 
-        //Part 3 B Control Panel  
+        //Part 3 A Control Panel  
         JButton headCb = new JButton("Control Panel for Latitude and Longitude: ");
-        headCb.setPreferredSize(new Dimension(1300, 50));
+        headCb.setPreferredSize(new Dimension(1350, 50));
         frame.getContentPane().add(headCb);
         frame.add(headCb);
         
@@ -199,10 +193,102 @@ public class GpsGui {
         frame.add(setR);
 
 
+        //Part 3 B Filtered Event  
+        JButton headCa = new JButton("Filtered Event: ");
+        headCa.setPreferredSize(new Dimension(1350, 50));
+        frame.getContentPane().add(headCa);
+        frame.add(headCa);
+        for(Stream<GpsEvent> s : streams)
+        {
+            s.listen((GpsEvent ev) -> { 
+            //Printing the GPS Data on the terminal
+            System.out.println(ev);
+             });
+
+            //Printing Tracker with numbers
+            Cell<String> tNum;
+            tNum = s.map((GpsEvent ev) -> 
+            {
+            String []expected;
+            //Using split to remove space to directly print Tracker
+            expected= ev.toString().split(" ");
+            String t1String;
+            //Printing Tracker as it is at place 0 in the array
+            t1String = (expected[0]);
+            //t1String is printing Tracker with number
+            return t1String;
+            //hold with no space  is used to make sure there is no hold in the data and it should get printing all together in the GUI 
+            }).hold("");
+            //Label for printing Latitude
+            SLabel lA = new SLabel(tNum);
+            lA.setPreferredSize(new Dimension(80, 30));
+            frame.add(lA);
+            Font ft = new Font("Serif", Font.BOLD, 16);
+            lA.setFont(ft);
+            lA.setForeground(Color.BLUE);
+            // lA.BorderLayout(5, 5);
+            
+
+
+            // Printing Latitude 
+            Cell<String> lNum;
+            lNum = s.map((GpsEvent ev) -> 
+            {
+            String []expected;
+            //Using split to remove space after Tracker to directly print Latitude
+            expected = ev.toString().split(" ");
+            String t2String;
+            //Printing Latitute as it is at place 2 in the array
+            t2String = (expected[2]);
+            //t2String is printing Latitude
+            return t2String;
+            //hold with no space  is used to make sure there is no hold in the data and it should get printing all together in the GUI  
+            }).hold("");
+            //Label for printing Latitude
+            SLabel lB = new SLabel(lNum);
+            frame.add(lB);
+            lB.setForeground(Color.RED);
+            lB.setPreferredSize(new Dimension(150, 30));
+            Font fLt = new Font("Serif", Font.BOLD, 16);
+            lB.setFont(fLt);
+
+            
+            // Printing Longitude 
+            Cell<String> lonNum;
+            lonNum = s.map((GpsEvent ev) -> 
+            {
+            String []expected;
+            //Using split to remove space after Longitude and directly print Longitude
+            expected = ev.toString().split(" ");
+            String t3String;
+            //Printing Longitude as it is at place 3 in the array
+            t3String = (expected[3]);
+            //t3String is printing Longitude
+            return t3String;  
+            //hold with no space  is used to make sure there is no hold in the data and it should get printing all together in the GUI
+            }).hold("");
+            //Label for printing Longitude
+            SLabel lC = new SLabel(lonNum);
+            Font fLot = new Font("Serif", Font.BOLD, 16);
+            lC.setFont(fLot);
+            lC.setForeground(Color.ORANGE);
+            lC.setPreferredSize(new Dimension(180, 30));
+            frame.add(lC);  
+
+              
+            //Layout for printing
+            frame.setLayout(new FlowLayout());
+            // Setting Gui as visible
+            frame.setVisible(true);
+        }
+
+
+        
+
 
         //Part 4  
         JButton headD = new JButton("Total distance travelled over the last 5 minutes by each tracker: ");
-        headD.setPreferredSize(new Dimension(1300, 50)); 
+        headD.setPreferredSize(new Dimension(1350, 50)); 
         frame.getContentPane().add(headD);
         frame.add(headD); 
 
