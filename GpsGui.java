ead1ad6b4b93f1a7499918c0cd36a9d495b7b7c9
Archie Verma <a1745070@student.adelaide.edu.au>
2022-11-07 21:35:28 +1030

    Added reference for the first GUI and changed the GUI frame name to guiFrame

--- GpsGui.java
+++ GpsGui.java
@@ -25,13 +25,13 @@ import javax.swing.border.*;
 public class GpsGui {
 
     public static void main(String[] args){
-
+         //Learned to create my first GUI: https://www.folkstalk.com/tech/jframe-frame-new-jframe-with-code-examples/
         //GUI frame heading
-        JFrame frame = new JFrame("GUI");
+        JFrame guiFrame = new JFrame("GUI");
         //Frame Size
-        frame.setSize(1350, 850);
+        guiFrame.setSize(1350, 850);
         //On Pressing GUI cross stop running the application
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        guiFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     
         
         //PART 1
@@ -42,7 +42,7 @@ public class GpsGui {
         JButton headA = new JButton("Trackers with their respected latitudes and longitudes below:");
         headA.setForeground(Color.BLACK);
         headA.setPreferredSize(new Dimension(1350, 40));
-        frame.getContentPane().add(headA);
+        guiFrame.getContentPane().add(headA);
 
 
         //Code to Attach a handler method to each stream from Example.java
@@ -59,7 +59,7 @@ public class GpsGui {
             {
             String []expected;
             //Using split to remove space to directly print Tracker
-            expected= ev.toString().split(" ");
+            expected= ev.toString().split(" "); 
             String t1String;
             //Printing Tracker as it is at place 0 in the array
             t1String = (expected[0]);
@@ -70,7 +70,7 @@ public class GpsGui {
             //Label for printing Latitude
             SLabel lA = new SLabel(tNum);
             lA.setPreferredSize(new Dimension(80, 30));
-            frame.add(lA);
+            guiFrame.add(lA);
             Font ft = new Font("Serif", Font.BOLD, 16);
             lA.setFont(ft);
             lA.setForeground(Color.BLUE);
@@ -94,7 +94,7 @@ public class GpsGui {
             }).hold("");
             //Label for printing Latitude
             SLabel lB = new SLabel(lNum);
-            frame.add(lB);
+            guiFrame.add(lB);
             lB.setForeground(Color.RED);
             lB.setPreferredSize(new Dimension(150, 30));
             Font fLt = new Font("Serif", Font.BOLD, 16);
@@ -121,104 +121,104 @@ public class GpsGui {
             lC.setFont(fLot);
             lC.setForeground(Color.ORANGE);
             lC.setPreferredSize(new Dimension(180, 30));
-            frame.add(lC);  
+            guiFrame.add(lC);  
 
               
             //Layout for printing
-            frame.setLayout(new FlowLayout());
+            guiFrame.setLayout(new FlowLayout());
             // Setting Gui as visible
-            frame.setVisible(true);
+            guiFrame.setVisible(true);
         }
 
         //Part 2 
         JButton headB = new JButton("Last Tracker Event time: ");
         headB.setPreferredSize(new Dimension(1350, 40));
-        frame.getContentPane().add(headB);
-        frame.add(headB); 
+        guiFrame.getContentPane().add(headB);
+        guiFrame.add(headB); 
         JLabel tK= new JLabel("Tracker0, latitude:39.977643, longitude:116.327703");
-        frame.add(tK);
+        guiFrame.add(tK);
         tK.setForeground(Color.BLUE);
         String dateTime = new SimpleDateFormat("dd/MM/yyyy, hh:mm:ss").format(new Date());
         JLabel  dC = new JLabel(dateTime);
         dC.setForeground(Color.BLUE);
-        frame.add(dC);
+        guiFrame.add(dC);
 
 
         //Part 3 A Control Panel  
         JButton headCb = new JButton("Control Panel for Latitude and Longitude: ");
         headCb.setPreferredSize(new Dimension(1350, 40));
-        frame.getContentPane().add(headCb);
-        frame.add(headCb);
+        guiFrame.getContentPane().add(headCb);
+        guiFrame.add(headCb);
         
         JLabel lLB = new JLabel("Latitude Lower Bound: ");
         lLB.setForeground(Color.BLUE);
-        frame.add(lLB);
+        guiFrame.add(lLB);
         lLB .setPreferredSize(new Dimension(150, 90));
 
 
         STextField textA = new STextField("-90.000", 7);
         textA.setPreferredSize(new Dimension(400, 30));
-        frame.add(textA);
+        guiFrame.add(textA);
         textA.setForeground(Color.BLUE);
         JLabel mkA = new JLabel("-90.000");
         mkA.setForeground(Color.RED);
-        frame.add(mkA);
+        guiFrame.add(mkA);
         mkA.setPreferredSize(new Dimension(60, 30));
 
         JLabel lUB = new JLabel("Latitude Upper Bound: ");
         lUB.setForeground(Color.BLUE);
-        frame.add(lUB );
+        guiFrame.add(lUB );
         lUB.setPreferredSize(new Dimension(150, 90));
 
         STextField textB = new STextField("90.000", 7);
         textB.setPreferredSize(new Dimension(400, 30));
-        frame.add(textB);
+        guiFrame.add(textB);
         textB.setForeground(Color.BLUE);
         JLabel mkB = new JLabel("90.000");
         mkB.setForeground(Color.RED);
-        frame.add(mkB);
+        guiFrame.add(mkB);
         mkB.setPreferredSize(new Dimension(60, 30));
 
         JLabel loLB = new JLabel("Longitude Lower Bound: ");
         loLB.setForeground(Color.BLUE);
-        frame.add(loLB );
+        guiFrame.add(loLB );
         loLB.setPreferredSize(new Dimension(160, 90));
 
         STextField textC = new STextField("-180.000", 7);
         textC.setPreferredSize(new Dimension(400, 30));
-        frame.add(textC);
+        guiFrame.add(textC);
         textC.setForeground(Color.BLUE);
         JLabel mkC = new JLabel("-180.000");
         mkC.setForeground(Color.RED);
-        frame.add(mkC);
+        guiFrame.add(mkC);
         mkC.setPreferredSize(new Dimension(60, 30));
 
         JLabel loUB = new JLabel("Longitudes Upper Bound: ");
         loUB.setForeground(Color.BLUE);
-        frame.add(loUB);
+        guiFrame.add(loUB);
         loUB .setPreferredSize(new Dimension(170, 90));
 
         STextField textD = new STextField("180.000", 7);
         textD.setPreferredSize(new Dimension(400, 30));
-        frame.add(textD);
+        guiFrame.add(textD);
         textD.setForeground(Color.BLUE);
         JLabel mkD = new JLabel("180.000");
         mkD.setForeground(Color.RED);
-        frame.add(mkD);
+        guiFrame.add(mkD);
         mkD.setPreferredSize(new Dimension(60, 30));
 
         JButton setR = new JButton("SET RANGE");
         setR.setPreferredSize(new Dimension(260, 30));
         setR.setForeground(Color.BLACK);
-        frame.getContentPane().add(setR);
-        frame.add(setR);
+        guiFrame.getContentPane().add(setR);
+        guiFrame.add(setR);
 
 
         //Part 3 B Filtered Event  
         JButton headCa = new JButton("Filtered Event: ");
         headCa.setPreferredSize(new Dimension(1350, 40));
-        frame.getContentPane().add(headCa);
-        frame.add(headCa);
+        guiFrame.getContentPane().add(headCa);
+        guiFrame.add(headCa);
         for(Stream<GpsEvent> s : streams)
         {
             s.listen((GpsEvent ev) -> { 
@@ -243,7 +243,7 @@ public class GpsGui {
             //Label for printing Latitude
             SLabel lA = new SLabel(tNum);
             lA.setPreferredSize(new Dimension(80, 30));
-            frame.add(lA);
+            guiFrame.add(lA);
             Font ft = new Font("Serif", Font.BOLD, 16);
             lA.setFont(ft);
             lA.setForeground(Color.RED);
@@ -267,7 +267,7 @@ public class GpsGui {
             }).hold("");
             //Label for printing Latitude
             SLabel lB = new SLabel(lNum);
-            frame.add(lB);
+            guiFrame.add(lB);
             lB.setForeground(Color.GREEN);
             lB.setPreferredSize(new Dimension(150, 30));
             Font fLt = new Font("Serif", Font.BOLD, 16);
@@ -294,13 +294,13 @@ public class GpsGui {
             lC.setFont(fLot);
             lC.setForeground(Color.BLUE);
             lC.setPreferredSize(new Dimension(180, 30));
-            frame.add(lC);  
+            guiFrame.add(lC);  
 
               
             //Layout for printing
-            frame.setLayout(new FlowLayout());
+            guiFrame.setLayout(new FlowLayout());
             // Setting Gui as visible
-            frame.setVisible(true);
+            guiFrame.setVisible(true);
         }
 
 
@@ -310,8 +310,8 @@ public class GpsGui {
         //Part 4  
         JButton headD = new JButton("Total distance travelled over the last 5 minutes by each tracker: ");
         headD.setPreferredSize(new Dimension(1350, 40)); 
-        frame.getContentPane().add(headD);
-        frame.add(headD); 
+        guiFrame.getContentPane().add(headD);
+        guiFrame.add(headD); 
 
 
 
@@ -342,32 +342,26 @@ public class GpsGui {
             dist.setPreferredSize(new Dimension(180, 20));
             Font ftA = new Font("Serif", Font.BOLD, 16);
             dist.setFont(ftA);
-            frame.add(dist);
+            guiFrame.add(dist);
             dist.setForeground(Color.RED); 
 
             SLabel lA = new SLabel(tNum);
             lA.setPreferredSize(new Dimension(70, 20));
-            frame.add(lA);
+            guiFrame.add(lA);
             Font ft = new Font("Serif", Font.BOLD, 16);
             lA.setFont(ft);
             lA.setForeground(Color.BLUE);
             JLabel dist0 = new JLabel("43m");
             dist0.setPreferredSize(new Dimension(70, 20));
-            frame.add(dist0);
+            guiFrame.add(dist0);
             dist0.setForeground(Color.YELLOW); 
             
               
             //Layout for printing
-            frame.setLayout(new FlowLayout());
+            guiFrame.setLayout(new FlowLayout());
             // Setting Gui as visible
-            frame.setVisible(true);
+            guiFrame.setVisible(true);
         } 
-
-
-       
-       
-
-
            
      }
  }
